44ecbc6543e31340e34e49516fac98d52065e63b
Valentino12341 <105175770+Valentino12341@users.noreply.github.com>
2026-03-24 16:10:11 +0100

    Add files via upload

---
+++ hm2.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+newarray = np.array([1,2,3,4,5,6,7,8,9])
+a=newarray.reshape(3,3)
+
+print(a[0:2,0:2])
+
+
+zevenroster = np.arange(1,50)
+b=zevenroster.reshape(7,7)
+print(b)
+print(b[2:5,2:5])
+
+var10 = np.arange(1,37)
+
+tenroster = np.arange(1,101)
+a = tenroster.reshape(10,10)
+print(a)
+
+middlehokje = a[2:7, 2:7]
+print(middlehokje)
+
+eerste_rij = a[0]
+print(eerste_rij)
+
+laatste_rij = a[:,-1]
+print(laatste_rij)
